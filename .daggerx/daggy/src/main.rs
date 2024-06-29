a0005794875991c7c60bce25051479e8ac1bf915
Alex Torres <atorres.ruiz@hotmail.com>
2024-06-29 13:43:41 +0200

    refactor: Update otel dependency versions to v1.27.0 (#97)
    - Update otel dependencies in go.mod to version 1.27.0.

--- .daggerx/daggy/src/main.rs
+++ .daggerx/daggy/src/main.rs
@@ -1,9 +1,11 @@
+use std::env;
 use std::fs::{self, File};
-use std::io::{Write, Error, ErrorKind};
+use std::io::{Write, Error, ErrorKind, Read};
 use std::path::Path;
 use std::process::{Command, Output, Stdio};
 use clap::Parser;
 use serde::Deserialize;
+use regex::Regex;
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -23,6 +25,8 @@ struct NewDaggerModule {
     name: String,
     github_actions_workflow_path: String,
     github_actions_workflow: String,
+    current_dir: String,
+    template_dir: String,
 }
 
 fn main() -> Result<(), Error> {
@@ -64,10 +68,7 @@ fn create_module(module: &str) -> Result<(), Error> {
     Ok(())
 }
 
-fn initialize_module(module_cfg: &NewDaggerModule) -> Result<(), Error> {
-    fs::create_dir_all(&module_cfg.path)?;
-    run_command_with_output(&format!("dagger init --sdk go --name {}", module_cfg.name), &module_cfg.path)?;
-
+fn update_dagger_json(module_cfg: &NewDaggerModule) -> Result<(), Error> {
     let dagger_json_path = format!("{}/dagger.json", module_cfg.path);
     let dagger_json_content = fs::read_to_string(&dagger_json_path)?;
     let new_dagger_json_content = dagger_json_content.replace(
@@ -76,14 +77,30 @@ fn initialize_module(module_cfg: &NewDaggerModule) -> Result<(), Error> {
         "exclude": ["../.direnv", "../.devenv", "../go.work", "../go.work.sum", "tests"]
     }"#,
     );
+
     fs::write(dagger_json_path, new_dagger_json_content)?;
 
+    Ok(())
+}
+
+fn initialize_module(module_cfg: &NewDaggerModule) -> Result<(), Error> {
+    // Creates the destination directory for the new module.
+    fs::create_dir_all(&module_cfg.path)?;
+    run_command_with_output(&format!("dagger init --sdk go --name {}", module_cfg.name), &module_cfg.path)?;
+
+    // Update dagger.json to exclude some files
+    update_dagger_json(module_cfg)?;
+
+    // Running dagger develop to initialize the module
     run_command_with_output(&format!("dagger develop -m {}", module_cfg.name), &module_cfg.path)?;
 
     let template_dir = ".daggerx/templates/module";
-    let destination_dir = &module_cfg.path;
+    let destination_dir = format!("{}/dagger", module_cfg.path);
 
-    copy_and_replace_templates(template_dir, destination_dir, &module_cfg.name)?;
+    fs::create_dir_all(&destination_dir)?;
+    copy_and_replace_templates(template_dir, &destination_dir, &module_cfg.name)?;
+
+    // replace_readme_content(module_cfg)?;
 
     Ok(())
 }
@@ -105,17 +122,25 @@ fn copy_and_replace_templates(template_dir: &str, destination_dir: &str, module_
                 replace_module_name(&content, module_name)
             };
 
-            let dest_path = format!("{}/{}", destination_dir, entry.file_name().to_string_lossy());
+            let dest_file_name = entry.file_name().to_string_lossy().replace(".tmpl", "");
+            let dest_path = format!("{}/{}", destination_dir, dest_file_name);
             fs::write(dest_path, new_content)?;
         }
     }
 
     Ok(())
 }
+
 fn initialize_tests(module_cfg: &NewDaggerModule) -> Result<(), Error> {
     let tests_path = format!("{}/tests", module_cfg.path);
     fs::create_dir_all(&tests_path)?;
     run_command_with_output("dagger init --sdk go --name tests", &tests_path)?;
+
+    // remove the main.go that's automatically generated if exists.
+    if Path::new(&format!("{}/main.go", tests_path)).exists() {
+        println!("Removing main.go from tests module");
+        fs::remove_file(format!("{}/main.go", tests_path))?;
+    }
 
     let tests_dagger_json_path = format!("{}/tests/dagger.json", module_cfg.path);
     let tests_dagger_json_content = fs::read_to_string(&tests_dagger_json_path)?;
@@ -128,7 +153,7 @@ fn initialize_tests(module_cfg: &NewDaggerModule) -> Result<(), Error> {
     fs::write(tests_dagger_json_path, new_tests_dagger_json_content)?;
 
     let test_template_dir = ".daggerx/templates/module/tests";
-    let test_destination_dir = format!("{}/tests", module_cfg.path);
+    let test_destination_dir = format!("{}/tests/dagger", module_cfg.path);  // Corrected destination path
 
     copy_and_replace_templates(&test_template_dir, &test_destination_dir, &module_cfg.name)?;
 
@@ -137,6 +162,7 @@ fn initialize_tests(module_cfg: &NewDaggerModule) -> Result<(), Error> {
 
     Ok(())
 }
+
 fn copy_readme_and_license(module_cfg: &NewDaggerModule) -> Result<(), Error> {
     fs::copy(".daggerx/templates/README.md", format!("{}/README.md", module_cfg.path))?;
     fs::copy(".daggerx/templates/LICENSE", format!("{}/LICENSE", module_cfg.path))?;
@@ -175,37 +201,49 @@ fn dagger_module_exists(module: &str) -> Result<NewDaggerModule, Error> {
         return Err(Error::new(ErrorKind::AlreadyExists, "Module already exists"));
     }
 
-    let module_path_full = std::env::current_dir()?.join(module);
-    let current_root_dir = std::env::current_dir()?;
+    let module_path_full = env::current_dir()?.join(module);
+    let current_root_dir = env::current_dir()?;
+
+    // Get the current dir, if the env var CURRENT_DIR_OVERRIDE is set, use that instead
+    let current_dir_override = env::var("CURRENT_DIR_OVERRIDE").unwrap_or("".to_string());
+    let current_dir = if current_dir_override.is_empty() {
+        current_root_dir.to_string_lossy().to_string()
+    } else {
+        current_dir_override
+    };
 
     Ok(NewDaggerModule {
         path: module_path_full.to_string_lossy().to_string(),
         name: module.to_string(),
         github_actions_workflow_path: current_root_dir.join(".github/workflows").to_string_lossy().to_string(),
         github_actions_workflow: current_root_dir.join(".github/workflows").join(format!("mod-{}-ci.yaml", module)).to_string_lossy().to_string(),
+        current_dir,
+        template_dir: ".daggerx/templates/module".to_string(),
     })
 }
 
-fn run_command_with_output(command: &str, current_dir: &str) -> Result<Output, Error> {
+fn run_command_with_output(command: &str, target_dir: &str) -> Result<Output, Error> {
     println!("Running command: {}", command);
-
     let output = Command::new("sh")
         .arg("-c")
         .arg(command)
-        .current_dir(current_dir)
+        .current_dir(target_dir)
         .stdout(Stdio::inherit())
         .stderr(Stdio::inherit())
         .output()?;
 
     if !output.status.success() {
-        return Err(Error::new(ErrorKind::Other, format!("Command failed with exit code: {}", output.status)));
+        return Err(Error::new(ErrorKind::Other, format!("Command failed with exit code: {} and with error: {}", output.status, String::from_utf8_lossy(&output.stderr))));
     }
 
     Ok(output)
 }
 
 fn replace_module_name(content: &str, module_name: &str) -> String {
-    content.replace("{{.module_name}}", module_name)
+    let capitalized_module_name = capitalize_module_name(module_name);
+    let re = Regex::new(r"\{\{\s*\.\s*module_name\s*\}\}").unwrap();
+
+    re.replace_all(content, &capitalized_module_name as &str).to_string()
 }
 
 fn capitalize_module_name(module_name: &str) -> String {
@@ -214,4 +252,32 @@ fn capitalize_module_name(module_name: &str) -> String {
         None => String::new(),
         Some(first) => first.to_uppercase().collect::<String>() + chars.as_str(),
     }
+}
+
+fn replace_readme_content(module_cfg: &NewDaggerModule) -> Result<(), Error> {
+    let readme_path = format!("{}/README.md", module_cfg.path);
+
+    // return an error if the README file does not exist
+    if !Path::new(&readme_path).exists() {
+        return Err(Error::new(ErrorKind::NotFound, format!("README.md file not found in {}", module_cfg.path)));
+    }
+
+    // search for the string {{ module_name }} in the README file
+    replace_content_in_file(&readme_path, &module_cfg.name)?;
+
+    Ok(())
+}
+
+fn replace_content_in_file(file_path: &str, content: &str) -> Result<(), Error> {
+    let mut file = File::open(file_path)?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+
+    let re = Regex::new(r"\{\{\s*\.\s*module_name\s*\}\}").unwrap();
+
+    let new_contents = re.replace_all(&contents, content).to_string();
+
+    fs::write(file_path, new_contents)?;
+
+    Ok(())
 }
