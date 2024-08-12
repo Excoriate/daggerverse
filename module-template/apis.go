1d9eefdd34abd7aa0ef5221a4fc16c19977e6a7d
Alex ATorres <atorres.ruiz@hotmail.com>
2024-08-12 13:14:00 +0200

    feat: Add Git and utilities installation for Ubuntu-based containers
    This commit introduces two new methods to the `ModuleTemplate` struct:
        
    1. `WithGitInUbuntuContainer()`: This method installs Git in the Ubuntu-based container.
    2. `WithUtilitiesInUbuntuContainer()`: This method updates the package lists and installs common utilities such as curl, wget, bash, jq, and vim in the Ubuntu-based container.
        
    These new methods provide an alternative to the existing `WithGitInAlpineContainer()` and `WithUtilitiesInAlpineContainer()` methods, which are specific to Alpine-based containers. This allows users to choose the appropriate container base image (Alpine or Ubuntu) and install the necessary tools accordingly.

--- module-template/apis.go
+++ module-template/apis.go
@@ -151,6 +151,19 @@ func (m *ModuleTemplate) WithGitInAlpineContainer() *ModuleTemplate {
 	return m
 }
 
+// WithGitInUbuntuContainer installs Git in the Ubuntu-based container.
+//
+// This method installs Git in the Ubuntu-based container.
+//
+// Returns:
+//   - *ModuleTemplate: The updated ModuleTemplate with Git installed in the container.
+func (m *ModuleTemplate) WithGitInUbuntuContainer() *ModuleTemplate {
+	m.Ctr = m.Ctr.
+		WithExec([]string{"apt-get", "install", "-y", "git"})
+
+	return m
+}
+
 // WithNewNetrcFileGitHub creates a new .netrc file with the GitHub credentials.
 //
 // The .netrc file is created in the root directory of the container.
@@ -213,6 +226,21 @@ func (m *ModuleTemplate) WithUtilitiesInAlpineContainer() *ModuleTemplate {
 	m.Ctr = m.Ctr.
 		WithExec([]string{"apk", "update"}).
 		WithExec([]string{"apk", "add", "curl", "wget", "bash", "jq", "vim"})
+
+	return m
+}
+
+// WithUtilitiesInUbuntuContainer installs common utilities in the Ubuntu-based container.
+//
+// This method updates the package lists for upgrades and installs the specified utilities
+// such as curl, wget, bash, jq, and vim in the Ubuntu-based container.
+//
+// Returns:
+//   - *ModuleTemplate: The updated ModuleTemplate with the utilities installed in the container.
+func (m *ModuleTemplate) WithUtilitiesInUbuntuContainer() *ModuleTemplate {
+	m.Ctr = m.Ctr.
+		WithExec([]string{"apt-get", "update"}).
+		WithExec([]string{"apt-get", "install", "-y", "curl", "wget", "bash", "jq", "vim"})
 
 	return m
 }
