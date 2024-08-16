eede278e68c7f86764a36a9ef96ac1c0e982760d
Alex ATorres <atorres.ruiz@hotmail.com>
2024-08-16 12:14:44 +0200

    feat: Add base image configuration for ModuleTemplate
    This commit introduces three new methods to the `ModuleTemplate` struct:
    
    1. `BaseAlpine`: Sets the base image to an Alpine Linux image and creates the base container. The version of the Alpine image can be specified, and it defaults to "latest" if not provided.
    
    2. `BaseUbuntu`: Sets the base image to an Ubuntu Linux image and creates the base container. The version of the Ubuntu image can be specified, and it defaults to "latest" if not provided.
    
    3. `BaseBusyBox`: Sets the base image to a BusyBox Linux image and creates the base container. The version of the BusyBox image can be specified, and it defaults to "latest" if not provided.
    
    These methods provide a convenient way to set the base image for the `ModuleTemplate` and ensure that the container is created with the desired base image. This change adds flexibility and allows users to choose the base image that best suits their needs.

---
+++ module-template/container.go
@@ -0,0 +1,69 @@
+package main
+
+import "fmt"
+
+const (
+	defaultAlpineImage  = "alpine"
+	defaultUbuntuImage  = "ubuntu"
+	defaultBusyBoxImage = "busybox"
+)
+
+// BaseAlpine sets the base image to an Alpine Linux image and creates the base container.
+//
+// Parameters:
+// - version: The version of the Alpine image to use. Optional parameter. Defaults to "latest".
+//
+// Returns a pointer to the ModuleTemplate instance.
+func (m *ModuleTemplate) BaseAlpine(
+	// version is the version of the Alpine image to use, e.g., "3.17.3".
+	// +optional
+	version string,
+) *ModuleTemplate {
+	if version == "" {
+		version = "latest"
+	}
+
+	imageURL := fmt.Sprintf("%s:%s", defaultAlpineImage, version)
+
+	return m.Base(imageURL)
+}
+
+// BaseUbuntu sets the base image to an Ubuntu Linux image and creates the base container.
+//
+// Parameters:
+// - version: The version of the Ubuntu image to use. Optional parameter. Defaults to "latest".
+//
+// Returns a pointer to the ModuleTemplate instance.
+func (m *ModuleTemplate) BaseUbuntu(
+	// version is the version of the Ubuntu image to use, e.g., "22.04".
+	// +optional
+	version string,
+) *ModuleTemplate {
+	if version == "" {
+		version = "latest"
+	}
+
+	imageURL := fmt.Sprintf("%s:%s", defaultUbuntuImage, version)
+
+	return m.Base(imageURL)
+}
+
+// BaseBusyBox sets the base image to a BusyBox Linux image and creates the base container.
+//
+// Parameters:
+// - version: The version of the BusyBox image to use. Optional parameter. Defaults to "latest".
+//
+// Returns a pointer to the ModuleTemplate instance.
+func (m *ModuleTemplate) BaseBusyBox(
+	// version is the version of the BusyBox image to use, e.g., "1.35.0".
+	// +optional
+	version string,
+) *ModuleTemplate {
+	if version == "" {
+		version = "latest"
+	}
+
+	imageURL := fmt.Sprintf("%s:%s", defaultBusyBoxImage, version)
+
+	return m.Base(imageURL)
+}
