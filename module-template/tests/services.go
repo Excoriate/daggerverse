4cb517f0dc70f4cc6fb535423cf8e32f3559fa04
Alex ATorres <atorres.ruiz@hotmail.com>
2024-08-17 18:15:01 +0200

    feat: Improve Go service with a new Dagger workflow
    - Add a new Dagger container for the Go service
    - Mount the test directory to the Go container workdir
    - Build and run the Go service within the container
    - Add a new Dagger container to interact with the Go service
    - Install curl in the client container and hit the service's API endpoint
    - Validate the API response contains the expected "Product A" string

--- module-template/tests/services.go
+++ module-template/tests/services.go
@@ -2,24 +2,60 @@ package main
 
 import (
 	"context"
+	"path/filepath"
+	"strings"
+
+	"github.com/Excoriate/daggerx/pkg/fixtures"
 
 	"github.com/Excoriate/daggerverse/module-template/tests/internal/dagger"
 )
 
 func (m *Tests) TestServiceSimple(ctx context.Context) error {
-	// Create a container for Go, so a Go service that exposes a port can be created.
-	goCtr := dag.Container().
-		From("golang:alpine")
-
-		// Initialize the target module.
+	// Initialize the target module.
 	targetModule := dag.ModuleTemplate()
 
+	// Create a container for Go, so a Go service that exposes a port can be created.
+	workdir := filepath.
+		Join(fixtures.MntPrefix, "goolang-service")
+
+	goCtr := dag.Container().
+		From("golang:alpine").
+		WithMountedDirectory(fixtures.MntPrefix, m.TestDir).
+		WithWorkdir(workdir).
+		WithExec([]string{"go", "build", "-o", "goolang-service"}).
+		WithExec([]string{"./goolang-service"})
+
 	// Configure the container as a service.
-	goSvc := targetModule.
+	goServer := targetModule.
 		WithServiceFromContainer(goCtr, dagger.
 			ModuleTemplateWithServiceFromContainerOpts{
 			ExposePorts: []int{8080},
 		})
+
+		// Initialize the go service in the go server (Dagger service)
+	clientCtr := dag.Container().
+		From("alpine:latest").
+		// Install curl
+		WithExec([]string{"apk", "add", "--no-cache", "curl"}).
+		Terminal().
+		WithServiceBinding("go-server", goServer)
+
+		// Hit the service's API endpoint
+	apiOut, apiErr := clientCtr.
+		WithExec([]string{"curl", "-s", "http://localhost:8080/products"}).
+		Stdout(ctx)
+
+	if apiErr != nil {
+		return WrapError(apiErr, "failed to get API response")
+	}
+
+	if apiOut == "" {
+		return Errorf("expected to have API response, got empty output")
+	}
+
+	if !strings.Contains(apiOut, "Product A") {
+		return Errorf("expected to have API response containing 'Product A', got %s", apiOut)
+	}
 
 	return nil
 }
