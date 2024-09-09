211da5b911b6dc10764763f289725515ab817c57
Alex ATorres <atorres.ruiz@hotmail.com>
2024-09-09 15:52:13 +0200

    fix: Remove the Apache License from the terragrunt repository
    Remove legacy tg module

--- terragrunt/dagger/config.go
+++
@@ -1,8 +0,0 @@
-package main
-
-const (
-	tgContainerImageDefault = "alpine/terragrunt"
-	tfVersionDefault        = "1.7.0"
-	tgCMD                   = "terragrunt"
-	tgNonInteractiveFlag    = "--terragrunt-non-interactive"
-)
