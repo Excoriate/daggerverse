c5a6143e3ad4ec5f10a3b3896894a2bdd39bd993
Alex ATorres <atorres.ruiz@hotmail.com>
2024-09-30 05:26:10 +0200

    feat: Add module configuration and template paths
    This commit introduces several changes to the `configuration.rs` module in the `daggerx/daggy` codebase:
    
    1. Adds new fields to the `NewDaggerModule` struct to store paths for module tests, GitHub Actions workflow, and template configurations.
    2. Introduces a new `TemplateCfg` struct to centralize the paths for various template files, including the GitHub Actions workflow CI template, module test template, and module template.
    3. Defines several constant strings to represent the paths for the GitHub Actions workflow directory, module tests directory, template directory, and the specific template file names.
    4. Updates the `get_module_configurations` function to initialize the new fields in the `NewDaggerModule` struct, including the `TemplateCfg` struct.
    
    These changes aim to improve the organization and maintainability of the module configuration and template handling, making it easier to manage and extend the project's structure in the future.

--- .daggerx/daggy/src/configuration.rs
+++ .daggerx/daggy/src/configuration.rs
@@ -10,7 +10,25 @@ pub struct NewDaggerModule {
     pub github_actions_workflow_path: String,
     pub github_actions_workflow: String,
     pub module_type: String,
+    pub template_cfg: TemplateCfg,
 }
+
+#[derive(Debug)]
+pub struct TemplateCfg {
+    pub github_actions_workflow_ci: String,
+    pub github_actions_workflow_ci_template: String,
+    pub module_test_template: String,
+    pub module_template: String,
+    pub module_light_template_dir: String,
+    pub module_full_template_dir: String,
+}
+
+const GITHUB_ACTIONS_WORKFLOW_DIR: &str = ".github/workflows";
+const MODULE_TESTS_DIR: &str = "tests";
+const TEMPLATE_DIR: &str = ".daggerx/templates";
+const GITHUB_ACTIONS_WORKFLOW_CI_TEMPLATE: &str = "mod-template-ci.yaml.tmpl";
+const MODULE_LIGHT_TEMPLATE_DIR: &str = "mod-light";
+const MODULE_FULL_TEMPLATE_DIR: &str = "mod-full";
 
 pub fn get_module_configurations(
     module: &str,
@@ -22,17 +40,53 @@ pub fn get_module_configurations(
     Ok(NewDaggerModule {
         path: module_path_full.to_string_lossy().to_string(),
         module_src_path: module_path_full.to_string_lossy().to_string(),
-        module_test_src_path: module_path_full.join("tests").to_string_lossy().to_string(),
+        module_test_src_path: module_path_full
+            .join(MODULE_TESTS_DIR)
+            .to_string_lossy()
+            .to_string(),
         name: module.to_string(),
         github_actions_workflow_path: current_root_dir
-            .join(".github/workflows")
+            .join(GITHUB_ACTIONS_WORKFLOW_DIR)
             .to_string_lossy()
             .to_string(),
         github_actions_workflow: current_root_dir
-            .join(".github/workflows")
+            .join(GITHUB_ACTIONS_WORKFLOW_DIR)
             .join(format!("ci-mod-{}.yaml", module))
             .to_string_lossy()
             .to_string(),
         module_type: module_type.to_string(),
+        template_cfg: TemplateCfg {
+            github_actions_workflow_ci: current_root_dir
+                .join(GITHUB_ACTIONS_WORKFLOW_DIR)
+                .join(GITHUB_ACTIONS_WORKFLOW_CI_TEMPLATE)
+                .to_string_lossy()
+                .to_string(),
+            github_actions_workflow_ci_template: current_root_dir
+                .join(TEMPLATE_DIR)
+                .join(GITHUB_ACTIONS_WORKFLOW_DIR)
+                .join(GITHUB_ACTIONS_WORKFLOW_CI_TEMPLATE)
+                .to_string_lossy()
+                .to_string(),
+            module_test_template: current_root_dir
+                .join(TEMPLATE_DIR)
+                .join(GITHUB_ACTIONS_WORKFLOW_CI_TEMPLATE)
+                .to_string_lossy()
+                .to_string(),
+            module_template: current_root_dir
+                .join(TEMPLATE_DIR)
+                .join(GITHUB_ACTIONS_WORKFLOW_CI_TEMPLATE)
+                .to_string_lossy()
+                .to_string(),
+            module_light_template_dir: current_root_dir
+                .join(TEMPLATE_DIR)
+                .join(MODULE_LIGHT_TEMPLATE_DIR)
+                .to_string_lossy()
+                .to_string(),
+            module_full_template_dir: current_root_dir
+                .join(TEMPLATE_DIR)
+                .join(MODULE_FULL_TEMPLATE_DIR)
+                .to_string_lossy()
+                .to_string(),
+        },
     })
 }
