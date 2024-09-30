09e50c99665e53e66c0cf2659a100bc33c1f313b
Alex ATorres <atorres.ruiz@hotmail.com>
2024-09-30 02:54:15 +0200

    feat: Move dagger module utilities to separate module
    The changes introduce a new `dagger_utils` module that contains two utility functions:
    
    1. `find_dagger_modules()`: This function searches for `dagger.json` files in the current directory and its subdirectories, and returns a vector of the parent directory paths of these files.
    
    2. `dagger_module_exists(module: &str)`: This function checks if a given module name already exists in the current directory. It returns an error if the module name is empty or if the module already exists.
    
    These utility functions were previously defined in the `main.rs` file and have been moved to the new `dagger_utils.rs` file to improve code organization and maintainability.

--- .daggerx/daggy/src/main.rs
+++ .daggerx/daggy/src/main.rs
@@ -3,6 +3,7 @@ mod command_utils;
 mod configuration;
 mod dagger_commands;
 mod dagger_json;
+mod dagger_utils;
 mod git;
 mod github_actions;
 mod naming;
@@ -24,10 +25,9 @@ use std::env;
 use std::fs;
 use std::io::{Error, ErrorKind};
 use std::path::Path;
-use std::process::Command;
 use templating::{copy_and_process_templates, replace_module_name};
 use utils::copy_dir_all;
-use regex::Regex;
+use dagger_utils::{find_dagger_modules, dagger_module_exists};
 
 fn main() -> Result<(), Error> {
     let args: Args = Args::parse();
@@ -332,48 +332,3 @@ fn initialize_tests(module_cfg: &NewDaggerModule) -> Result<(), Error> {
 }
 
 
-fn dagger_module_exists(module: &str) -> Result<(), Error> {
-    if module.is_empty() {
-        return Err(Error::new(
-            ErrorKind::InvalidInput,
-            "Module name cannot be empty",
-        ));
-    }
-
-    // Check if the module already exists in the root of this directory.
-    if Path::new(module).exists() {
-        return Err(Error::new(
-            ErrorKind::AlreadyExists,
-            "Module already exists",
-        ));
-    }
-
-    Ok(())
-}
-
-
-fn find_dagger_modules() -> Result<Vec<String>, Error> {
-    let output = Command::new("find")
-        .args(&[".", "-type", "f", "-name", "dagger.json"])
-        .output()?;
-
-    if !output.status.success() {
-        return Err(Error::new(
-            ErrorKind::Other,
-            "Failed to execute find command",
-        ));
-    }
-
-    let modules = String::from_utf8_lossy(&output.stdout)
-        .lines()
-        .map(|line| {
-            Path::new(line)
-                .parent()
-                .unwrap()
-                .to_string_lossy()
-                .into_owned()
-        })
-        .collect::<Vec<String>>();
-
-    Ok(modules)
-}
