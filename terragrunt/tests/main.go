68b155377b9d428f97ee525f2316cadc9d6a9998
Alex ATorres <atorres.ruiz@hotmail.com>
2024-10-13 14:33:01 +0200

    feat: Add test for Terragrunt exec with plan output
    This commit adds a new test case to the `TestAll` function in the `main.go` file of the `terragrunt/tests` package. The new test case, `TestTerragruntExecWithPlanOutput`, verifies the behavior of the Terragrunt `exec` command when it is executed with the `--plan-output-path` option. This change enhances the test suite to cover more use cases for the Terragrunt `exec` command.

--- terragrunt/tests/main.go
+++ terragrunt/tests/main.go
@@ -71,6 +71,7 @@ func (m *Tests) TestAll(ctx context.Context) error {
 	polTests.Go(m.TestTerragruntExecVersionCommand)
 	polTests.Go(m.TestTerragruntExecPlanCommand)
 	polTests.Go(m.TestTerragruntExecLifecycleCommands)
+	polTests.Go(m.TestTerragruntExecWithPlanOutput)
 
 	if err := polTests.Wait(); err != nil {
 		return WrapError(err, "there are some failed tests")
