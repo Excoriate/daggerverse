676be82f8f28d9c5f730e763faeeb0c90c769795
Alex ATorres <atorres.ruiz@hotmail.com>
2024-11-03 09:28:15 +0100

    feat: Add Alpine container with utilities
    Add support for configuring an Alpine container with common utilities like curl.
    This includes:
    - A test to verify the presence of the 'curl' utility in the Alpine container.
    - Support for configuring an Ubuntu container with utilities.
    - A test to verify the presence of the 'curl' utility in the Ubuntu container.
    - Support for configuring an Alpine container with Git.
    - A test to verify the presence of Git in the Alpine container.

---
+++ tflinter/apis.go
@@ -0,0 +1,480 @@
+// Package main provides methods for setting up and managing a container environment.
+// This includes setting environment variables, mounting directories and files,
+// and configuring services such as Docker within the container.
+//
+// Copyright: Excoriate alex_torres@outlook.com
+// License: MIT
+package main
+
+import (
+	"context"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"github.com/Excoriate/daggerverse/tflinter/internal/dagger"
+
+	"github.com/Excoriate/daggerx/pkg/fixtures"
+)
+
+// WithEnvironmentVariable sets an environment variable in the container.
+//
+// Parameters:
+//   - name: The name of the environment variable (e.g., "HOST").
+//   - value: The value of the environment variable (e.g., "localhost").
+//   - expand: Whether to replace `${VAR}` or $VAR in the value according to the current
+//     environment variables defined in the container (e.g., "/opt/bin:$PATH").
+//     Optional parameter.
+func (m *Tflinter) WithEnvironmentVariable(
+	// name is the name of the environment variable.
+	name string,
+	// value is the value of the environment variable.
+	value string,
+	// expand is whether to replace `${VAR}` or $VAR in the value according to the current
+	// +optional
+	expand bool,
+) *Tflinter {
+	m.Ctr = m.Ctr.WithEnvVariable(name, value, dagger.ContainerWithEnvVariableOpts{
+		Expand: expand,
+	})
+
+	return m
+}
+
+// WithSource sets the source directory for the container.
+//
+// Parameters:
+// - src: The directory that contains all the source code, including the module directory.
+// - workdir: The working directory within the container. Optional parameter.
+func (m *Tflinter) WithSource(
+	// src is the directory that contains all the source code, including the module directory.
+	src *dagger.Directory,
+	// workdir is the working directory within the container. If not set it'll default to /mnt
+	// +optional
+	workdir string,
+) *Tflinter {
+	ctr := m.Ctr.WithMountedDirectory(fixtures.MntPrefix, src)
+
+	if workdir != "" {
+		ctr = ctr.WithWorkdir(filepath.Join(fixtures.MntPrefix, workdir))
+	} else {
+		ctr = ctr.WithWorkdir(fixtures.MntPrefix)
+	}
+
+	m.Ctr = ctr
+
+	return m
+}
+
+// WithContainer sets the container to be used.
+//
+// Parameters:
+// - ctr: The container to run the command in. If passed, it will override the container set in the Dagger instance.
+func (m *Tflinter) WithContainer(
+	ctr *dagger.Container,
+) *Tflinter {
+	m.Ctr = ctr
+
+	return m
+}
+
+// WithFileMountedInContainer adds a file to the container.
+//
+// Parameters:
+// - file: The file to add to the container.
+// - dest: The destination path in the container. Optional parameter.
+// - owner: The owner of the file. Optional parameter.
+func (m *Tflinter) WithFileMountedInContainer(
+	file *dagger.File,
+	dest string,
+	owner string,
+) *Tflinter {
+	path := filepath.Join(fixtures.MntPrefix, dest)
+	if owner != "" {
+		m.Ctr = m.Ctr.WithMountedFile(path, file, dagger.ContainerWithMountedFileOpts{
+			Owner: owner,
+		})
+
+		return m
+	}
+
+	m.Ctr = m.Ctr.WithMountedFile(path, file)
+
+	return m
+}
+
+// WithSecretAsEnvVar sets an environment variable in the container using a secret.
+//
+// Parameters:
+//   - name: The name of the environment variable (e.g., "API_KEY").
+//   - secret: The secret containing the value of the environment variable.
+//
+// Returns:
+//   - *Tflinter: The updated Tflinter with the environment variable set.
+//
+// Behavior:
+//   - The secret value is expanded according to the current environment variables defined in the container.
+func (m *Tflinter) WithSecretAsEnvVar(name string, secret *dagger.Secret) *Tflinter {
+	secretValue, err := secret.Plaintext(context.Background())
+	if err != nil {
+		return nil
+	}
+
+	m.Ctr = m.Ctr.WithEnvVariable(name, secretValue, dagger.ContainerWithEnvVariableOpts{
+		Expand: true,
+	})
+
+	return m
+}
+
+// WithDownloadedFile downloads a file from the specified URL and mounts it in the container.
+//
+// Parameters:
+//   - url: The URL of the file to download.
+//   - destDir: The directory within the container where the file will be downloaded. Optional parameter.
+//     If not provided, it defaults to the predefined mount prefix.
+//
+// Returns:
+//   - *Tflinter: The updated Tflinter with the downloaded file mounted in the container.
+func (m *Tflinter) WithDownloadedFile(
+	// url is the URL of the file to download.
+	url string,
+	// destFileName is the name of the file to download. If not set, it'll default to the basename of the URL.
+	// +optional
+	destFileName string,
+) *Tflinter {
+	// Extract the filename from the last part of the URL.
+	fileName := filepath.Base(url)
+	if destFileName != "" {
+		fileName = destFileName
+	}
+
+	// Download the file
+	fileDownloaded := dag.HTTP(url).WithName(fileName)
+
+	// Define the path in the container
+	destFilePath := filepath.Join(fixtures.MntPrefix, fileName)
+
+	// Mount the file in the container
+	m.Ctr = m.
+		Ctr.
+		WithMountedFile(destFilePath, fileDownloaded)
+
+	return m
+}
+
+// WithClonedGitRepoHTTPS clones a Git repository and mounts it as a directory in the container.
+//
+// This method downloads a Git repository and mounts it as a directory in the container. It supports optional
+// authentication tokens for private repositories and can handle both GitHub and GitLab repositories.
+//
+// Parameters:
+//   - repoURL: The URL of the git repository to clone (e.g., "https://github.com/user/repo").
+//   - token: (Optional) The VCS token to use for authentication. If
+//     not provided, the repository will be cloned without authentication.
+//   - vcs: (Optional) The version control system (VCS) to use for
+//     authentication. Defaults to "github". Supported values are "github" and "gitlab".
+//
+// Returns:
+//   - *Tflinter: The updated Tflinter with the cloned repository mounted in the container.
+func (m *Tflinter) WithClonedGitRepoHTTPS(
+	repoURL string,
+	// token is the VCS token to use for authentication. Optional parameter.
+	// +optional
+	token *dagger.Secret,
+	// vcs is the VCS to use for authentication. Optional parameter.
+	// +optional
+	vcs string,
+	// authHeader is the authentication header to use for authentication. Optional parameter.
+	// +optional
+	authHeader *dagger.Secret,
+	// returnDir is a string that indicates the directory path of the repository to return.
+	// +optional
+	returnDir string,
+	// branch is the branch to checkout. Optional parameter.
+	// +optional
+	branch string,
+	// discardGitDir is a boolean that indicates if the .git directory should be discarded. Optional parameter.
+	// +optional
+	discardGitDir bool,
+	// tag is the tag to checkout. Optional parameter.
+	// +optional
+	tag string,
+	// commit is the commit to checkout. Optional parameter.
+	// +optional
+	commit string,
+) *Tflinter {
+	// Call the helper function to clone the repository.
+	clonedRepo := m.CloneGitRepoHTTPS(
+		repoURL,
+		token,
+		vcs,
+		authHeader,
+		returnDir,
+		branch,
+		discardGitDir,
+		tag,
+		commit,
+	)
+
+	// Mount the cloned repository as a directory inside the container.
+	m.Ctr = m.Ctr.WithMountedDirectory(fixtures.MntPrefix, clonedRepo)
+
+	return m
+}
+
+// WithClonedGitRepoSSH clones a git repository using SSH for authentication.
+//
+// Parameters:
+//   - repoURL: The URL of the git repository to clone.
+//   - sshSocket: The SSH socket to use for authentication.
+//   - sshKnownHosts: The known hosts to use for authentication. Optional parameter.
+//   - returnDir: A string that indicates the directory path of the repository to return. Optional parameter.
+//   - branch: The branch to checkout. Optional parameter.
+//   - keepGitDir: A boolean that indicates if the .git directory should be kept. Optional parameter.
+//
+// Returns:
+//   - *Tflinter: The updated Tflinter with the cloned repository mounted in the container.
+func (m *Tflinter) WithClonedGitRepoSSH(
+	// repoURL is the URL of the git repository to clone.
+	repoURL string,
+	// sshAuthSocket is the SSH socket to use for authentication.
+	sshAuthSocket *dagger.Socket,
+	// sshKnownHosts is the known hosts to use for authentication. Optional parameter.
+	// +optional
+	sshKnownHosts string,
+	// returnDir is a string that indicates the directory path of the repository to return. Optional parameter.
+	// +optional
+	returnDir string,
+	// branch is the branch to checkout. Optional parameter.
+	// +optional
+	branch string,
+	// discardGitDir is a boolean that indicates if the .git directory should be discarded. Optional parameter.
+	// +optional
+	discardGitDir bool,
+	// tag is the tag to checkout. Optional parameter.
+	// +optional
+	tag string,
+	// commit is the commit to checkout. Optional parameter.
+	// +optional
+	commit string,
+) *Tflinter {
+	// Call the helper function to clone the repository.
+	clonedRepo := m.CloneGitRepoSSH(
+		repoURL,
+		sshAuthSocket,
+		sshKnownHosts,
+		returnDir,
+		branch,
+		discardGitDir,
+		tag,
+		commit,
+	)
+
+	// Mount the cloned repository as a directory inside the container.
+	m.Ctr = m.
+		Ctr.
+		WithMountedDirectory(fixtures.MntPrefix, clonedRepo)
+
+	return m
+}
+
+// WithCacheBuster sets a cache-busting environment variable in the container.
+//
+// This method sets an environment variable "CACHE_BUSTER" with a timestamp value in RFC3339Nano format.
+// This can be useful for invalidating caches by providing a unique value.
+//
+// Returns:
+//   - *Tflinter: The updated Tflinter with the cache-busting environment variable set.
+func (m *Tflinter) WithCacheBuster() *Tflinter {
+	m.Ctr = m.Ctr.
+		WithEnvVariable("CACHE_BUSTER", time.
+			Now().
+			Format(time.RFC3339Nano))
+
+	return m
+}
+
+// WithConfigFile mounts a configuration file into the container at the specified path.
+//
+// This method allows you to mount a configuration file into the container at a specified path.
+// If no path is provided, it defaults to a predefined mount prefix from the fixtures package.
+//
+// Args:
+//   - cfgPath (string): The path where the config file will be mounted. If empty, it defaults to fixtures.MntPrefix.
+//     +optional
+//   - cfgFile (*dagger.File): The config file to be mounted.
+//
+// Returns:
+//   - *Tflinter: The updated Tflinter with the config file mounted in the container.
+func (m *Tflinter) WithConfigFile(
+	// cfgFile is the config file to be mounted.
+	cfgFile *dagger.File,
+	// cfgPathInCtr is the path where the config file will be mounted.
+	// +optional
+	cfgPathInCtr string,
+	// setEnvVar is a string that set an environment variable in the container with the config file path.
+	// +optional
+	setEnvVar string,
+) *Tflinter {
+	if cfgPathInCtr == "" {
+		cfgPathInCtr = fixtures.MntPrefix
+	}
+
+	m.Ctr = m.Ctr.
+		WithMountedFile(cfgPathInCtr, cfgFile)
+
+	if setEnvVar != "" {
+		setEnvVar = strings.ToUpper(strings.TrimSpace(setEnvVar))
+		setEnvVar = strings.ReplaceAll(setEnvVar, "\\", "")
+		m.Ctr = m.Ctr.WithEnvVariable(setEnvVar, strings.TrimSpace(cfgPathInCtr))
+	}
+
+	return m
+}
+
+// WithCachedDirectory mounts a cache volume in the container.
+//
+// Parameters:
+// - path: The path in the container where the cache volume will be mounted.
+// - cacheVolume: The cache volume to mount.
+func (m *Tflinter) WithCachedDirectory(
+	// path is the path in the container where the cache volume will be mounted.
+	path string,
+	// enablePrefixWithMountPath is whether to enable the prefix with the mount path.
+	// +optional
+	enablePrefixWithMountPath bool,
+	// setEnvVarWithCacheDirValue is the value to set the cache directory in the container.
+	// +optional
+	setEnvVarWithCacheDirValue string,
+	// cacheSharingMode is the sharing mode of the cache volume.
+	// +optional
+	cacheSharingMode dagger.CacheSharingMode,
+	// cacheVolumeRootDir is the root directory of the cache volume.
+	// +optional
+	cacheVolumeRootDir *dagger.Directory,
+	// cacheVolumeOwner is the owner of the cache volume.
+	// +optional
+	cacheVolumeOwner string,
+) *Tflinter {
+	// Define the cache volume
+	cacheVolume := dag.CacheVolume(path)
+
+	// Define the path in the container
+	mountPath := path
+	if enablePrefixWithMountPath {
+		mountPath = filepath.Join(fixtures.MntPrefix, path)
+	}
+
+	cacheMountOpts := dagger.ContainerWithMountedCacheOpts{}
+	if cacheVolumeRootDir != nil {
+		cacheMountOpts.Source = cacheVolumeRootDir
+	}
+
+	if cacheVolumeOwner != "" {
+		cacheMountOpts.Owner = cacheVolumeOwner
+	}
+
+	if cacheSharingMode != "" {
+		cacheMountOpts.Sharing = cacheSharingMode
+	}
+
+	// Mount the cache volume in the container
+	m.Ctr = m.Ctr.
+		WithMountedCache(mountPath, cacheVolume, cacheMountOpts)
+
+	// Set the environment variable if provided
+	if setEnvVarWithCacheDirValue != "" {
+		envVarName := strings.ToUpper(strings.TrimSpace(setEnvVarWithCacheDirValue))
+		m.Ctr = m.
+			Ctr.
+			WithEnvVariable(envVarName, mountPath)
+	}
+
+	return m
+}
+
+// WithUserAsOwnerOfDirs sets the specified user (and optionally group) as the owner
+// of the given directories within the container.
+//
+// This method iterates over the provided list of directories and executes the "chown" command
+// within the container to change the ownership of each directory to the specified user (and optionally group).
+//
+// Parameters:
+//   - user: The user to set as the owner of the directories. This should be a valid user within the container.
+//   - group: The group to set as the owner of the directories. This is an optional parameter.
+//   - dirs: A slice of strings representing the directories to set the owner of. Each directory path should be
+//     specified relative to the container's filesystem.
+//
+// Returns:
+// - *Terragrunt: The updated Terragrunt instance with the ownership of the specified directories changed.
+func (m *Tflinter) WithUserAsOwnerOfDirs(
+	// user is the user to set as the owner of the directories.
+	user string,
+	// dirs is the directories to set the owner of.
+	dirs []string,
+	// group is the group to set as the owner of the directories.
+	// +optional
+	group string,
+	// configureAsRoot is whether to configure the directories as root, and then it'll use the given user.
+	// +optional
+	configureAsRoot bool,
+) *Tflinter {
+	if configureAsRoot {
+		m.Ctr = m.Ctr.WithUser("root")
+	}
+
+	for _, dir := range dirs {
+		if group != "" {
+			m.Ctr = m.Ctr.WithExec([]string{"chown", "-R", user + ":" + group, dir})
+		} else {
+			m.Ctr = m.Ctr.WithExec([]string{"chown", "-R", user, dir})
+		}
+	}
+
+	if configureAsRoot {
+		m.Ctr = m.Ctr.WithUser(user)
+	}
+
+	return m
+}
+
+// WithUserWithPermissionsOnDirs sets the specified permissions on the given directories within the container.
+//
+// This method iterates over the provided list of directories and executes the "chmod" command
+// within the container to change the permissions of each directory to the specified mode.
+//
+// Parameters:
+//   - user: The user to set as the owner of the directories. This should be a valid user within the container.
+//   - mode: The permissions to set on the directories. This should be a valid mode string (e.g., "0777").
+//   - dirs: A slice of strings representing the directories to set the permissions of. Each directory path should be
+//     specified relative to the container's filesystem.
+//   - configureAsRoot: Whether to configure the directories as root, and then it'll use the given mode.
+//
+// Returns:
+// - *Terragrunt: The updated Terragrunt instance with the permissions of the specified directories changed.
+func (m *Tflinter) WithUserWithPermissionsOnDirs(
+	// user is the user to set as the owner of the directories.
+	user string,
+	// mode is the permissions to set on the directories.
+	mode string,
+	// dirs is the directories to set the permissions of.
+	dirs []string,
+	// configureAsRoot is whether to configure the directories as root, and then it'll use the given mode.
+	// +optional
+	configureAsRoot bool,
+) *Tflinter {
+	if configureAsRoot {
+		m.Ctr = m.Ctr.WithUser("root")
+	}
+
+	for _, dir := range dirs {
+		m.Ctr = m.Ctr.WithExec([]string{"chmod", "-R", mode, dir})
+	}
+
+	if configureAsRoot && user != "" {
+		m.Ctr = m.Ctr.WithUser(user)
+	}
+
+	return m
+}
---
+++ tflinter/tests/utils.go
@@ -0,0 +1,199 @@
+package main
+
+import (
+	"context"
+	"strings"
+
+	"github.com/Excoriate/daggerverse/tflinter/tests/internal/dagger"
+)
+
+const (
+	defaultTestDir = "testdata"
+)
+
+// getTestDir returns the test directory.
+//
+// This is a helper method for tests, in order to get the test directory which
+// is located in the same directory as the test file, and normally named as "testdata".
+func (m *Tests) getTestDir(testDir string) *dagger.Directory {
+	if testDir == "" {
+		testDir = defaultTestDir
+	}
+
+	return dag.
+		CurrentModule().
+		Source().
+		Directory(testDir)
+}
+
+// assertVersionOfBinaryInContainer executes the version command for a given tool and checks
+// the output to ensure it contains the expected version string. If the command fails or the output does not contain
+// the expected string, an error is returned.
+//
+//nolint:unused // This is a helper method for tests, and it's not used in the tests.
+func (m *Tests) assertVersionOfBinaryInContainer(
+	ctx context.Context,
+	ctr *dagger.Container,
+	tool string,
+	expectedVersion string,
+	versionCmd string,
+) error {
+	if versionCmd == "" {
+		versionCmd = "--version"
+	}
+
+	versionOut, versionOutErr := ctr.
+		WithExec([]string{tool, versionCmd}).
+		Stdout(ctx)
+
+	if versionOutErr != nil {
+		return WrapErrorf(versionOutErr, "failed to get %s version output", tool)
+	}
+
+	if versionOut == "" {
+		return Errorf("%s version output is empty", tool)
+	}
+
+	if !strings.Contains(versionOut, expectedVersion) {
+		return Errorf("%s version is expected to contain '%s', but it doesn't", tool, expectedVersion)
+	}
+
+	return nil
+}
+
+// assertEnvVarIsSetInContainer checks if a specific environment variable is set in the container.
+// It executes the "printenv" command in the container and verifies if the output contains the specified variable.
+// If the command fails, the output is empty, or the variable is not found, an error is returned.
+//
+//nolint:unused // This is a helper method for tests, and it's not used in the tests.
+func (m *Tests) assertEnvVarIsSetInContainer(
+	ctx context.Context,
+	ctr *dagger.Container,
+	variable string,
+) error {
+	variableOut, variableOutErr := ctr.
+		WithExec([]string{"printenv"}).
+		Stdout(ctx)
+
+	if variableOutErr != nil {
+		return WrapErrorf(variableOutErr, "failed to get environment variable '%s' output", variable)
+	}
+
+	if variableOut == "" {
+		return Errorf("environment variable '%s' is not set", variable)
+	}
+
+	if !strings.Contains(variableOut, variable) {
+		return Errorf("environment variable '%s' is expected to contain '%s', but it doesn't", variable, variableOut)
+	}
+
+	return nil
+}
+
+// assertTheseFoldersExistsInContainer checks if the specified folders are present in the container.
+// It executes the "ls -la <folder>" command in the container for each folder in the list.
+// If the command fails or the output is empty for any folder, an error is returned.
+//
+//nolint:unused // This is a helper method for tests, and it's not used in the tests.
+func (m *Tests) assertTheseFoldersExistsInContainer(
+	ctx context.Context,
+	ctr *dagger.Container,
+	folders []string,
+) error {
+	// list all, and pwd.
+	ctr = ctr.
+		WithExec([]string{"pwd"}).
+		WithExec([]string{"ls", "-la"})
+
+	for _, folder := range folders {
+		// Use 'test -d' to check if the folder exists
+		stdout, err := ctr.
+			WithExec([]string{"test", "-d", folder}).
+			Stdout(ctx)
+
+		if err != nil {
+			return WrapErrorf(err, "failed to check if folder '%s' exists", folder)
+		}
+
+		// If 'test -d' succeeds, it doesn't produce any output
+		// So, an empty stdout means the folder exists
+		if stdout != "" {
+			return Errorf("unexpected output when checking folder '%s': %s", folder, stdout)
+		}
+
+		// Optionally, list the contents of the folder
+		folderContents, err := ctr.
+			WithExec([]string{"ls", "-la", folder}).
+			Stdout(ctx)
+
+		if err != nil {
+			return WrapErrorf(err, "failed to list contents of folder '%s'", folder)
+		}
+
+		if folderContents == "" {
+			return Errorf("folder '%s' exists but is empty", folder)
+		}
+	}
+
+	return nil
+}
+
+// assertTheseFilesExistsInContainer checks if the specified files are present in the container.
+// It uses 'test -f' to check file existence.
+//
+//nolint:unused // This is a helper method for tests, and it's not used in the tests.
+func (m *Tests) assertTheseFilesExistsInContainer(
+	ctx context.Context,
+	ctr *dagger.Container,
+	files []string,
+) error {
+	for _, file := range files {
+		// Check if file exists
+		stdout, err := ctr.
+			WithExec([]string{"test", "-f", file}).
+			Stdout(ctx)
+
+		if err != nil {
+			return WrapErrorf(err, "failed to check if file '%s' exists", file)
+		}
+
+		if stdout != "" {
+			return Errorf("unexpected output when checking file '%s': %s", file, stdout)
+		}
+	}
+
+	return nil
+}
+
+// assertFileContentShouldContain checks if the specified file in the container contains the given content.
+//
+//nolint:unused // This is a helper method for tests, and it's not used in the tests.
+func (m *Tests) assertFileContentShouldContain(
+	ctx context.Context,
+	ctr *dagger.Container,
+	file string,
+	content string,
+) error {
+	if err := m.assertTheseFilesExistsInContainer(ctx, ctr, []string{file}); err != nil {
+		return err
+	}
+
+	fileOut, fileOutErr := ctr.
+		WithExec([]string{"cat", file}).
+		Stdout(ctx)
+
+	if fileOutErr != nil {
+		return WrapErrorf(fileOutErr, "failed to get file '%s' content", file)
+	}
+
+	if fileOut == "" {
+		return Errorf("file '%s' content is empty", file)
+	}
+
+	if !strings.Contains(fileOut, content) {
+		return Errorf("file '%s' content is expected to contain '%s', but its current content is '%s'",
+			file, content, fileOut)
+	}
+
+	return nil
+}
